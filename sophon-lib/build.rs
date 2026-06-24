766c5c1de7b089d920ac052ef763d18df91a0236
Nikita Podvirnyi <krypt0nn@vk.com>
2026-06-24 23:06:31 +0200

    chore: move hpatchz

--- sophon-lib/build.rs
+++ sophon-lib/build.rs
@@ -1,21 +1,4 @@
 fn main() {
-    // if cfg!(feature = "vendored-hpatchz") {
-    //     use md5::{Md5, Digest};
-
-    //     println!("cargo::rerun-if-changed=external/hpatchz/hpatchz");
-
-    //     let buf = std::fs::read("external/hpatchz/hpatchz")
-    //         .expect("failed to read hpatchz binary");
-
-    //     let mut hash = [0; 16];
-
-    //     let hash = Md5::default()
-    //         .chain_update(&buf)
-    //         .finalize();
-
-    //     println!("cargo::rustc-env=HPATCHZ_MD5={hash:x}");
-    // }
-
     prost_build::Config::new()
         .compile_protos(
             &[
